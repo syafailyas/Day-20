b6192ee852ca4fca9148483f871510a1bf9c0013
kinara <kinara.varga@outlook.com>
2023-04-17 08:59:12 +0700

    first commit

---
+++ 00FileBuffering/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+class Program {
+    static void Main() {
+        string path = "bootcamplargefile.txt";
+
+        using (FileStream fs = new FileStream(path, FileMode.Open))
+        using (BufferedStream bufferedStream = new BufferedStream(fs)) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = bufferedStream.Read(buffer, 0, buffer.Length)) > 0) {
+                // process the buffer
+            }
+        }
+    }
+}
